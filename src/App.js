ce23091e03ddf12f905085b27cac4cacd29e2e2a
Aditi Konde <aditi.konde.25@gmail.com>
2020-03-23 14:24:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container bg-warning">
+        <h3>Hello</h3>
+        <TodoList />
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <h3>hello from todo-item</h3>
+      </div>
+    );
+  }
+}
